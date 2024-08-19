fa7349c37970f0f64adfae5f10640eceac4edfb8
matiss jurevics <matissjurevics@gmail.com>
2024-08-19 20:34:11 +0100

    updated description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,14 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Cartshare - Share your cart in real time",
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Cartshare is a plugin for shopify stores that lets users create a shared cart and add items into it in real time. This allows users to reach free shipping faster as well as obtain discounts.' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
       script: [
         // {src: "https://getlaunchlist.com/js/widget-diy.js", defer: true},
       ]
