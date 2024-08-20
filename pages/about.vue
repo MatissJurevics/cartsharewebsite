3230fdb21d080ebe16acdbf299cf648fbd0e0ec4
matiss jurevics <matissjurevics@gmail.com>
2024-08-20 14:17:47 +0100

    fixed the about us page

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,10 @@
 <template>
-    <section class="h-[100vh] px-20 py-12">
+    <section class="min-h-[100vh] px-20 py-12">
         <h1 class="text-6xl font-bold text-[#ff5470]">Meet The Team</h1>
-        <div class="mt-24">
-            <div class="flex flex-row justify-between">
+        <div class="mt-24 h-fit">
+            <div class="flex flex-col lg:flex-row justify-between gap-12">
                 <div class="flex flex-col justify-center items-center">
-                    <img class="w-64 mx-auto rounded-full aspect-square" src="https://media.licdn.com/dms/image/v2/D4E03AQEESAVQp0ZdbA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1696001180906?e=1729728000&v=beta&t=u1rRKzOyEy9Gw6Wwf8COhOzZAqLbG4daVEXieqy0pNU" alt="">
+                    <img class="w-32 lg:w-64 mx-auto rounded-full aspect-square" src="https://media.licdn.com/dms/image/v2/D4E03AQEESAVQp0ZdbA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1696001180906?e=1729728000&v=beta&t=u1rRKzOyEy9Gw6Wwf8COhOzZAqLbG4daVEXieqy0pNU" alt="">
 
                     <h2 class="text-2xl font-bold text-center mt-8">Matiss Jurevics</h2>
                     <a href="https://www.linkedin.com/in/matiss-jurevics-121162240/">
@@ -12,14 +12,14 @@
                     </a>
                 </div>
                 <div class="flex flex-col justify-center items-center">
-                    <img class="w-64 mx-auto rounded-full aspect-square" src="https://media.licdn.com/dms/image/D4E03AQG1SVbj29RYUw/profile-displayphoto-shrink_400_400/0/1713358102608?e=1729728000&v=beta&t=NE0qkXp1qEuQdFQJnLDOqXswyBrSB-xi9PSNMsfgEew" alt="">
+                    <img class="w-32 lg:w-64 mx-auto rounded-full aspect-square" src="https://media.licdn.com/dms/image/D4E03AQG1SVbj29RYUw/profile-displayphoto-shrink_400_400/0/1713358102608?e=1729728000&v=beta&t=NE0qkXp1qEuQdFQJnLDOqXswyBrSB-xi9PSNMsfgEew" alt="">
                     <h2 class="text-2xl font-bold text-center mt-8">Liam Fuller</h2>
                     <a href="https://www.linkedin.com/in/liam-fuller-499637283/">
                         <svg class="h-12 w-12 cursor-pointer" fill="#ffffff80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
                     </a>
                 </div>
                 <div class="flex flex-col justify-center items-center">
-                    <img class="w-64 mx-auto rounded-full aspect-square" src="/public/keith.jpg" alt="">
+                    <img class="w-32 lg:w-64 mx-auto rounded-full aspect-square" src="/public/keith.jpg" alt="">
 
                     <h2 class="text-2xl font-bold text-center mt-8">Keith Salhani</h2>
                     <a href="https://www.linkedin.com/in/keith-salhani/">
